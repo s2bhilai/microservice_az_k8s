ff98a8344c0daabba815d6da9eca75199bc319b9
Subin <subinbs4a@gmail.com>
2022-02-12 10:30:55 +0300

    Shopping Client project setup

---
+++ Shopping/Shopping.Client/Data/ProductContext.cs
@@ -0,0 +1,24 @@
+﻿using Shopping.Client.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shopping.Client.Data
+{
+    public static class ProductContext
+    {
+        public static readonly List<Product> products = new List<Product>
+        {
+            new Product()
+            {
+                Name="IPhone X",
+                Description = "asdfasdf",
+                ImageFile="p-1.png",
+                Price=345.00M,
+                Category="Smart Phone"
+            }
+        };
+    }
+}
